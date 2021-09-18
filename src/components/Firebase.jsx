ff2de99db864e775e2d44491c96e497784e5c469
inz2020 <>
2021-09-18 16:30:43 +0200

    update du fichier index.css et du fichier App.jsx. Configuration de la BD firebase par le fichier firebase.js

---
+++ src/components/Firebase.jsx
@@ -0,0 +1,13 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCsh1ZV_DYBTLWTugGpZifAUO1wghIQ4Zk",
+  authDomain: "gofachat.firebaseapp.com",
+  projectId: "gofachat",
+  storageBucket: "gofachat.appspot.com",
+  messagingSenderId: "338943327097",
+  appId: "1:338943327097:web:c5245e68736bd6d1070da8"
+
+};
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-import {GoogleOutlined, facebookOutlined} from "@ant-design/icons"
+import {GoogleOutlined, FacebookOutlined} from "@ant-design/icons"
 
 const Login = () => {
     return (
         <div id="login-page">
-            Hello
+           <div id="login-card">
+               <h2>Welcome  to gofaChat!!</h2>
+               <div className="login-button google">
+                   <GoogleOutlined/> Sign In with Google
+               </div>
+          <br/> <br/>
+               <div className="login-button facebook">
+                   <FacebookOutlined/> Sign In with Facebook
+               </div>
+           </div>
         </div>
     );
 };
